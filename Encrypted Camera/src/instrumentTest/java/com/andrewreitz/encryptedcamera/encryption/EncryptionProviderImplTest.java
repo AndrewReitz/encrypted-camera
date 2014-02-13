81a94ae2a4a8545f1e9688c46e2c23c01a6786bb
Andrew Reitz <aj.reitz@gmail.com>
2014-02-12 23:27:34 -0600

    getting the hang of unit tests

--- Encrypted Camera/src/instrumentTest/java/com/andrewreitz/encryptedcamera/encryption/EncryptionProviderImplTest.java
+++ Encrypted Camera/src/instrumentTest/java/com/andrewreitz/encryptedcamera/encryption/EncryptionProviderImplTest.java
@@ -1,8 +1,15 @@
 package com.andrewreitz.encryptedcamera.encryption;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 /**
  * @author Andrew
  */
 public class EncryptionProviderImplTest {
 
+    @Test public void shouldDoSomething() {
+        throw new RuntimeException("TEST");
+    }
 }
--- Encrypted Camera/src/instrumentTest/java/com/andrewreitz/encryptedcamera/encryption/KeyManagerImplTest.java
+++ Encrypted Camera/src/instrumentTest/java/com/andrewreitz/encryptedcamera/encryption/KeyManagerImplTest.java
@@ -2,9 +2,6 @@ package com.andrewreitz.encryptedcamera.encryption;
 
 import android.test.AndroidTestCase;
 
-import org.junit.Before;
-import org.junit.Test;
-
 import java.io.IOException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -17,12 +14,11 @@ public class KeyManagerImplTest extends AndroidTestCase {
 
     KeyManager keyManager;
 
-    @Before
-    void setup() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
+    public void setup() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
         keyManager = new KeyManagerImpl(getContext());
     }
 
-    @Test void shouldDoSomeStuff() {
+    public void shouldDoSomeStuff() {
         throw new RuntimeException("CRASHED");
     }
 }
