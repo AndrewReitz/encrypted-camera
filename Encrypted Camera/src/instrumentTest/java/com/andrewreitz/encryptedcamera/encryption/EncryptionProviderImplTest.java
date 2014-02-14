0e89513482f7a87586d85503eb53cbc872dfa0e6
Andrew Reitz <aj.reitz@gmail.com>
2014-02-13 23:30:41 -0600

    Implemented KeyManagerUnitTests

--- Encrypted Camera/src/instrumentTest/java/com/andrewreitz/encryptedcamera/encryption/EncryptionProviderImplTest.java
+++ Encrypted Camera/src/instrumentTest/java/com/andrewreitz/encryptedcamera/encryption/EncryptionProviderImplTest.java
@@ -1,15 +1,7 @@
 package com.andrewreitz.encryptedcamera.encryption;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
 /**
  * @author Andrew
  */
 public class EncryptionProviderImplTest {
-
-    @Test public void shouldDoSomething() {
-        throw new RuntimeException("TEST");
-    }
 }
--- Encrypted Camera/src/instrumentTest/java/com/andrewreitz/encryptedcamera/encryption/KeyManagerImplTest.java
+++ Encrypted Camera/src/instrumentTest/java/com/andrewreitz/encryptedcamera/encryption/KeyManagerImplTest.java
@@ -2,23 +2,72 @@ package com.andrewreitz.encryptedcamera.encryption;
 
 import android.test.AndroidTestCase;
 
+import java.io.File;
 import java.io.IOException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.SecretKey;
+
+import static org.fest.assertions.api.Assertions.assertThat;
 
 /**
  * @author areitz
  */
+@SuppressWarnings("ConstantConditions")
 public class KeyManagerImplTest extends AndroidTestCase {
 
-    KeyManager keyManager;
+    private String KEY_STORE_NAME = "test.keystore";
+    private KeyManager keyManager;
 
-    public void setup() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
-        keyManager = new KeyManagerImpl(getContext());
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        keyManager = new KeyManagerImpl(KEY_STORE_NAME, getContext());
     }
 
-    public void shouldDoSomeStuff() {
-        throw new RuntimeException("CRASHED");
+    public void testPreconditions() {
+        assertThat(keyManager).isNotNull();
+    }
+
+    public void testSaveKeyStore() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
+        keyManager.saveKeyStore();
+        File file = getContext().getFileStreamPath(KEY_STORE_NAME);
+        assertThat(file).exists();
+    }
+
+    public void testShouldGenerateKeyWithPasswordSaveKeyAndGetKey() throws InvalidKeySpecException, NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException {
+        // Arrange
+        String password = "test";
+        String salt = "temp";
+        String alias = "test";
+
+        // Act
+        SecretKey secretKey = keyManager.generateKeyWithPassword(password.toCharArray(), salt.getBytes());
+        keyManager.saveKey(alias, secretKey);
+        SecretKey key = keyManager.getKey(alias);
+
+        // Assert
+        assertThat(key.getAlgorithm()).isEqualTo(secretKey.getAlgorithm());
+        assertThat(key.getEncoded()).isEqualTo(secretKey.getEncoded());
+        assertThat(key.getFormat()).isEqualTo(secretKey.getFormat());
+    }
+
+    public void testShouldGenerateKeySaveKeyAndGetKey() throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException {
+        // Arrange
+        String alias = "test";
+
+        // Act
+        SecretKey secretKey = keyManager.generateKeyNoPassword();
+        keyManager.saveKey(alias, secretKey);
+        SecretKey key = keyManager.getKey(alias);
+
+        // Assert
+        assertThat(key.getAlgorithm()).isEqualTo(secretKey.getAlgorithm());
+        assertThat(key.getEncoded()).isEqualTo(secretKey.getEncoded());
+        assertThat(key.getFormat()).isEqualTo(secretKey.getFormat());
     }
 }
