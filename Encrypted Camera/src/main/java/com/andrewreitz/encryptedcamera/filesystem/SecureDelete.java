d4372c6f4c26dd60cd614ad447dc895a952a06a1
Andrew Reitz <aj.reitz@gmail.com>
2014-02-15 14:57:17 -0600

    added comments

--- Encrypted Camera/src/main/java/com/andrewreitz/encryptedcamera/filesystem/SecureDelete.java
+++ Encrypted Camera/src/main/java/com/andrewreitz/encryptedcamera/filesystem/SecureDelete.java
@@ -7,5 +7,13 @@ import java.io.IOException;
  * @author areitz
  */
 public interface SecureDelete {
+
+    /**
+     * Writes random bytes to the file then deletes it to make it unrecoverable.
+     *
+     * @param file the file to delete
+     * @return true if the file was deleted or if the file never existed
+     * @throws IOException
+     */
     boolean secureDelete(File file) throws IOException;
 }
