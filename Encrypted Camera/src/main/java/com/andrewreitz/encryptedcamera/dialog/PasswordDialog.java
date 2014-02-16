7c624352e6bd05e5e62f75dcb782f8bb30f0396f
Andrew Reitz <aj.reitz@gmail.com>
2014-02-16 00:01:36 -0600

    implementing password dialog

---
+++ Encrypted Camera/src/main/java/com/andrewreitz/encryptedcamera/dialog/PasswordDialog.java
@@ -0,0 +1,61 @@
+package com.andrewreitz.encryptedcamera.dialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import com.andrewreitz.encryptedcamera.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * @author areitz
+ */
+public class PasswordDialog extends DialogFragment implements AlertDialog.OnClickListener, DialogInterface.OnShowListener {
+
+    @InjectView(R.id.dialog_editText_password) EditText passwordEditText;
+
+    @Override public Dialog onCreateDialog(Bundle savedInstanceState) {
+        @SuppressWarnings("ConstantConditions")
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        View view = inflater.inflate(R.layout.dialog_password, null);
+        ButterKnife.inject(this, view);
+        builder.setView(view)
+                .setPositiveButton(android.R.string.ok, this)
+                .setNegativeButton(android.R.string.cancel, this);
+        AlertDialog dialog = builder.create();
+        dialog.setOnShowListener(this);
+        return dialog;
+    }
+
+    @Override public void onClick(DialogInterface dialog, int which) {
+        switch (which) {
+            case DialogInterface.BUTTON_POSITIVE:
+                break;
+            case DialogInterface.BUTTON_NEGATIVE:
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown button pressed, which == " + which);
+        }
+    }
+
+    @Override public void onShow(DialogInterface dialog) {
+
+    }
+
+    public interface PasswordDialogListener {
+        void onPasswordEntered(String password);
+        void onPasswordCancel();
+    }
+}
